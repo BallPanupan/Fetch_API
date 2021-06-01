17c39fa6e508ba6a1707b9273a3f29fd6b5f3d10
Ball <ball@reverseads.com>
2021-06-01 13:43:28 +0700

    delete comment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 fetch('https://app.dropoffer.com/v1/api/contacts/savedproperties/4483193000000435376')
   .then(function (response) {
-    return response.json() // แปลงข้อมูลที่ได้เป็น json
+    return response.json() 
   })
   .then(function (response) {
-    console.log("status : ", response.status); // แสดงข้อมูล JSON จาก then ข้างบน
+    console.log("status : ", response.status); 
     console.log("ZIP : ", response.data['Zip']);
     console.log("Owner : ", response.data['Owner'].name);
 
